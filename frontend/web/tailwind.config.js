d7003b721ef5f8611a7322d1de309d5b130ff4f0
Ptisuiisse <van-sluys.loic@orange.fr>
2025-05-10 15:02:30 +0200

    feat: Initial setup of Docker ervices (frontend, backend, nginx) - INIT backend with Fastify. -  CONF Nginx - Create Dockerfiles for frontend, backend, and Nginx. - Frontend accessible at http://localhost/ - Backend accessible at http://localhost/api/

---
+++ frontend/web/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // Si vous avez un fichier HTML principal
+    "./src/**/*.{js,ts,jsx,tsx}", // Pour tous les fichiers JS/TS/JSX/TSX dans le dossier src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

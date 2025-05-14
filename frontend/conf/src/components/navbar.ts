612cf74cdd25cc12ef72b12f38a482eb433536c7
Ptisuisse <van-sluys.loic@orange.fr>
2025-05-14 13:26:30 +0000

    feat: add hamburger & responsive
    -  menu with dropdown links
    - improve responsive design

--- frontend/conf/src/components/navbar.ts
+++ frontend/conf/src/components/navbar.ts
@@ -1,70 +1,95 @@
 export function createNavbar(routes: { [key: string]: string }): HTMLElement {
+  const nav = document.createElement('nav');
+  nav.className = 'bg-[#242424] p-4 text-white flex justify-between items-center fixed top-0 left-0 right-0 w-full z-50';
 
-	const nav = document.createElement('nav');
-	nav.className = 'bg-[#242424] p-4 text-white flex justify-center items-center fixed top-0 left-0 right-0 w-full z-50'; 
+  // Conteneur gauche et droit
+  const leftItemsContainer = document.createElement('div');
+  leftItemsContainer.className = 'absolute left-4 top-1/2 transform -translate-y-1/2 flex items-center space-x-4'; 
+
+  const rightItemsContainer = document.createElement('div');
+  rightItemsContainer.className = 'absolute right-4 top-1/2 transform -translate-y-1/2 flex items-center space-x-4'; 
+
+  // texte ft_transcendence
+  const textLogoLink = document.createElement('a');
+  textLogoLink.href = '/'; 
+  textLogoLink.setAttribute('data-link', ''); 
+  textLogoLink.textContent = 'ft_transcendence';
+  textLogoLink.className = `
+    transition-all duration-300
+    hover:scale-90 transform  text-xl font-bold`;
+  leftItemsContainer.appendChild(textLogoLink);
+
+  // image login droit
+  const loginImageLink = document.createElement('a');
+  loginImageLink.href = '/login';
+  loginImageLink.setAttribute('data-link', '');
   
-	// Conteneur pour element gauche et droit
-	const leftItemsContainer = document.createElement('div');
-	leftItemsContainer.className = 'absolute left-4 top-1/2 transform -translate-y-1/2 flex items-center space-x-4'; // 'space-x-4' ajoute de l'espace entre les logos
-	const rightItemsContainer = document.createElement('div');
-	rightItemsContainer.className = 'absolute right-4 top-1/2 transform -translate-y-1/2 flex items-center space-x-4'; // 'space-x-4' ajoute de l'espace entre les logos
+  const loginImg = document.createElement('img');
+  const defaultLoginSrc = '/login.png';
+  const hoverLoginSrc = '/hover_login.png';
 
+  loginImg.src = defaultLoginSrc; 
+  loginImg.alt = 'Login';
+  loginImg.className = 'h-7 w-auto';
 
-	// texte "ft_transcendence"
-	const textLogoLink = document.createElement('a');
-	textLogoLink.href = '/'; 
-	textLogoLink.setAttribute('data-link', ''); 
-	textLogoLink.textContent = 'ft_transcendence';
-	textLogoLink.className = `
-	transition-all duration-300
-	hover:scale-90 transform  text-xl font-bold`;
-	leftItemsContainer.appendChild(textLogoLink);
+  loginImageLink.addEventListener('mouseenter', () => {
+    loginImg.src = hoverLoginSrc;
+    loginImg.className = `h-8`;
+  });
+
+  loginImageLink.addEventListener('mouseleave', () => {
+    loginImg.src = defaultLoginSrc;
+    loginImg.className = 'h-7';
+  });
   
-	// image login cote droit
-	const loginImageLink = document.createElement('a');
-	loginImageLink.href = '/login';
-	loginImageLink.setAttribute('data-link', '');
+  loginImageLink.appendChild(loginImg);
+  rightItemsContainer.appendChild(loginImageLink);
+  nav.appendChild(rightItemsContainer);
+  nav.appendChild(leftItemsContainer);
+
+  // Conteneur pour les liens de navigation
+  const navLinks = document.createElement('div');
+  navLinks.className = 'navbar-links'; // utilise le layer defini dans le style.css facon tailwind
   
-	const loginImg = document.createElement('img');
-    const defaultLoginSrc = '/login.png';
-    const hoverLoginSrc = '/hover_login.png';
+  for (const path in routes) {
+    const link = document.createElement('a');
+    link.href = path;
+    link.textContent = routes[path];
+    link.setAttribute('data-link', '');
+    link.className = `
+      text-lg transition-all duration-300 
+      hover:text-[#C3BABA] hover:font-bold 
+      hover:scale-125 transform 
+    `;
+    navLinks.appendChild(link);
+  }
 
-    loginImg.src = defaultLoginSrc; 
-    loginImg.alt = 'Login';
-    loginImg.className = 'h-7 w-auto';
-
-    loginImageLink.addEventListener('mouseenter', () => {
-        loginImg.src = hoverLoginSrc;
-		loginImg.className = `h-8`;
-    });
-
-    loginImageLink.addEventListener('mouseleave', () => {
-        loginImg.src = defaultLoginSrc;
-		loginImg.className = 'h-7';
-    });
-    
-	loginImageLink.appendChild(loginImg);
-	rightItemsContainer.appendChild(loginImageLink);
-	nav.appendChild(rightItemsContainer);
-	nav.appendChild(leftItemsContainer);
-	
-	const navLinks = document.createElement('div');
- 	navLinks.className = 'md:flex space-x-10 items-center'
+  nav.appendChild(navLinks);
+ 
+	// A retenir toggle ajoute une classe remove la retire
+  // Ajout hamburger
+  const hamburgerBtn = document.createElement('button');
+  hamburgerBtn.className = 'hamburger-btn'; //  utilise le layer defini dans le style.css facon tailwind
+  hamburgerBtn.innerHTML = '☰'; // Icone hamburger
   
-	  for (const path in routes) {
-		const link = document.createElement('a');
-		link.href = path;
-		link.textContent = routes[path];
-		link.setAttribute('data-link', '');
-		link.className = `
-		text-lg transition-all duration-300 
-		hover:text-[#C3BABA] hover:font-bold 
-		hover:scale-125 transform 
-	  `;
-		navLinks.appendChild(link);
-	  }
-	
-	  nav.appendChild(navLinks);
-	
-	  return nav;
+  // affichage du menu au clic
+  hamburgerBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('open'); // classe open definie dans le style.css
+  });
+
+  nav.appendChild(hamburgerBtn);
+
+  // Eventlistener ecoute pour verifier la taille de la window
+  window.addEventListener('resize', () => {
+    if (window.innerWidth >= 768) {
+      navLinks.classList.remove('open');
+    }
+  });
+
+  // comportement au chargement de la page
+  if (window.innerWidth >= 768) { 
+    navLinks.classList.remove('open');
+  }
+
+  return nav;
 }
--- frontend/conf/src/pages/main.ts
+++ frontend/conf/src/pages/main.ts
@@ -2,21 +2,14 @@ import '../style.css';
 
 export function HomePage(): HTMLElement {
     const mainContainer = document.createElement('div');
-    // Ce conteneur prendra la hauteur et largeur disponibles sous la navbar.
-    // 'flex' permettra d'aligner les enfants (colonnes).
-    // 'min-h-[calc(100vh-4rem)]' pour la hauteur, 'w-full' pour la largeur.
     mainContainer.className = 'page-container-full-height flex border border-red-500'; 
-    // Création de trois colonnes
+
     const column1 = document.createElement('div');
-    // 'flex-1' permet à chaque colonne de prendre une part égale de l'espace.
-    // Ajoutez des bordures et du padding pour la visibilité.
+
     column1.className = 'flex-1 border border-gray-300 p-4';
 
     const column2 = document.createElement('div');
-    column2.className = 'flex-1 border border-gray-300 p-4'; // Couleur de fond différente pour la distinction
-	const rond = document.createElement('div');
-	rond.className = 'flex-1 border border-blue-300 p-10';
-	column2.appendChild(rond);
+    column2.className = 'flex-1 border border-gray-300 p-4';
     const column3 = document.createElement('div');
     column3.className = 'flex-1 border border-gray-500 p-4';
 

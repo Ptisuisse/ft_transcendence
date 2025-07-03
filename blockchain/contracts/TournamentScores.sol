5e2b45becb1c07e75778433032f7dd707918c3cc
Ptisuisse <van-sluys.loic@orange.fr>
2025-07-03 19:14:09 +0200

    continue some tests

--- blockchain/contracts/TournamentScores.sol
+++ blockchain/contracts/TournamentScores.sol
@@ -8,31 +8,21 @@ contract TournamentScores {
         uint256 timestamp;
     }
 
-    // Tableau de scores pour chaque tournoi (id => scores)
-    mapping(uint256 => Score[]) private tournamentScores;
+    Score[] public scores;
 
-    // Event pour notifier l’ajout d’un score
-    event ScoreAdded(uint256 indexed tournamentId, address indexed player, uint256 score, uint256 timestamp);
+    event ScoreSubmitted(address indexed player, uint256 score, uint256 timestamp);
 
-    // Ajouter un score à un tournoi
-    function addScore(uint256 tournamentId, uint256 score) public {
-        tournamentScores[tournamentId].push(Score(msg.sender, score, block.timestamp));
-        emit ScoreAdded(tournamentId, msg.sender, score, block.timestamp);
+    function submitScore(uint256 _score) public {
+        scores.push(Score(msg.sender, _score, block.timestamp));
+        emit ScoreSubmitted(msg.sender, _score, block.timestamp);
     }
 
-    // Récupérer le nombre de scores pour un tournoi
-    function getScoresCount(uint256 tournamentId) public view returns (uint256) {
-        return tournamentScores[tournamentId].length;
-    }
-
-    // Récupérer un score précis d’un tournoi
-    function getScore(uint256 tournamentId, uint256 index) public view returns (address, uint256, uint256) {
-        Score memory s = tournamentScores[tournamentId][index];
+    function getScore(uint256 index) public view returns (address, uint256, uint256) {
+        Score memory s = scores[index];
         return (s.player, s.score, s.timestamp);
     }
 
-    // Récupérer tous les scores d’un tournoi (attention, peut être coûteux en gas si beaucoup de scores)
-    function getAllScores(uint256 tournamentId) public view returns (Score[] memory) {
-        return tournamentScores[tournamentId];
+    function getScoresCount() public view returns (uint256) {
+        return scores.length;
     }
 }
